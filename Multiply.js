f5a46141aab40129135f94b33de78945534b41d8
Daniel Calmon Feitoza <57994600+danfeitoza@users.noreply.github.com>
2022-06-06 10:20:35 -0300

    Desafio Multiply.js - CodeWars

---
+++ Multiply.js
@@ -0,0 +1,24 @@
+Desafio pede para que seja verificado o que há de errrado com o código abaixo:
+Challenge asks to verify what's wrong with the code below:
+Le defi demande de vérifier ce qui ne va pas avec le code ci-dessous:
+
+function multiply(a, b){
+a * b
+}
+
+A resposta coreta é:
+The correct answer is:
+La bonne réponse est:
+
+function multiply(a, b){
+return a * b
+}
+
+Pois os valores não estavam sendo retornados.
+Because the values weren't returned.
+Parce que les valeurs n'ont pas été renvoyées.
+"A declaração return finaliza a execução de uma função e especifica os valores que devem ser retonados para onde a função foi chamada."
+"The declaration
+"The return statement ends function execution and specifies a value to be returned to the function caller."
+"L'instruction return met fin à l'exécution d'une fonction et définit une valeur à renvoyer à la fonction appelante."
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/return
